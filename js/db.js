3f72db11360b2da74fb2a508e35f3b38f876bcbe
jgonzalezma <josu0805@gmail.com>
2020-01-13 10:43:50 +0100

    mongodb conectado correctamente

--- js/db.js
+++ js/db.js
@@ -1,7 +1,12 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamdvbnphbGV6bWEiLCJhIjoiY2s1Nmw3dDkxMDVzMjNlcG1zZWV6Y2h1bCJ9.MFxOeTeuyFurtNlQOEJYkQ', {
-attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-maxZoom: 18,
-id: 'mapbox/streets-v11',
-accessToken: 'pk.eyJ1IjoiamdvbnphbGV6bWEiLCJhIjoiY2s1Nmw3dDkxMDVzMjNlcG1zZWV6Y2h1bCJ9.MFxOeTeuyFurtNlQOEJYkQ'
-}).addTo(mymap);
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mapa");
+  dbo.collection("mapa").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
