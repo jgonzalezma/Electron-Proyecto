4eb9a5404501fc3c5449ddedb8fc9f900c9ce293
jgonzalezma <josu0805@gmail.com>
2020-01-14 12:38:35 +0100

    controles en el mapa

--- js/map.js
+++ js/map.js
@@ -1,13 +1,12 @@
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+var mymap = L.map('map', {drawControl:true}).setView([43.29, -1.98], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamdvbnphbGV6bWEiLCJhIjoiY2s1YzhrNmxuMDVjODNucWYyemxzaGZzOSJ9.8D3kII0_gLRI2XS3jKCDAA', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoiamdvbnphbGV6bWEiLCJhIjoiY2s1YzhrNmxuMDVjODNucWYyemxzaGZzOSJ9.8D3kII0_gLRI2XS3jKCDAA'
 }).addTo(mymap);
 
 //Marcadores
-var marker = L.marker([43.3, -1.98]).addTo(mymap);
+var marker = L.marker([43.29, -1.98]).addTo(mymap);
 
 //Circulos
 var circle = L.circle([51.508, -0.11], {
@@ -28,8 +27,8 @@ var polygon = L.polygon([
 marker.bindPopup("<b>Punto 1</b><br>I am a popup.").openPopup();
 
 //Clicks en el mapa
-function onMapClick(e) {
+/* function onMapClick(e) {
 alert("Has clickado en " + e.latlng);
 }
 
-mymap.on('click', onMapClick);
+mymap.on('click', onMapClick); */
--- js/requires.js
+++ js/requires.js
@@ -1,2 +1,1 @@
 require("leaflet");
-var leafletDraw = require('leaflet-draw');
