1465829bd09aa98162e3c84f10333b74976b3bbf
jgonzalezma <josu0805@gmail.com>
2020-01-15 09:59:31 +0100

    guarda los marcadores en la bd correctamente

--- js/db.js
+++ js/db.js
@@ -6,10 +6,8 @@ MongoClient.connect(url, function(err, db) {
   var dbo = db.db("mapa");
   dbo.collection("mapa").find({}).toArray(function(err, result) {
     if (err) throw err;
-    console.log(result[2].coordinates);
-    console.log(result[2].desc)
-    var marker2 = L.marker(result[2].coordinates).addTo(mymap);
-    marker2.bindPopup("<b>"+result[2].desc+"</b>").openPopup();
+    var marker2 = L.marker(result[0].coordinates).addTo(map);
+    marker2.bindPopup("<b>"+result[0].desc+"</b>").openPopup();
     db.close();
   });
 });
--- js/map.js
+++ js/map.js
@@ -1,12 +1,12 @@
-var mymap = L.map('map', {drawControl:true}).setView([43.29, -1.98], 13);
+var map = L.map('map', {drawControl:true}).setView([43.29, -1.98], 12);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamdvbnphbGV6bWEiLCJhIjoiY2s1YzhrNmxuMDVjODNucWYyemxzaGZzOSJ9.8D3kII0_gLRI2XS3jKCDAA', {
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoiamdvbnphbGV6bWEiLCJhIjoiY2s1YzhrNmxuMDVjODNucWYyemxzaGZzOSJ9.8D3kII0_gLRI2XS3jKCDAA'
-}).addTo(mymap);
+}).addTo(map);
 
 //Marcadores
-var marker = L.marker([43.29, -1.98]).addTo(mymap);
+var marker = L.marker([43.29, -1.98]).addTo(map);
 
 //Circulos
 var circle = L.circle([51.508, -0.11], {
@@ -14,14 +14,54 @@ color: 'red',
 fillColor: '#f03',
 fillOpacity: 0.5,
 radius: 500
-}).addTo(mymap);
+}).addTo(map);
 
 //Poligonos
 var polygon = L.polygon([
 [51.509, -0.08],
 [51.503, -0.06],
 [51.51, -0.047]
-]).addTo(mymap);
+]).addTo(map);
 
 //Popups
 marker.bindPopup("<b>Punto 1</b><br>I am a popup.").openPopup();
+
+//Ver escala
+L.control.scale().addTo(map);
+
+function onMapClick(e) {
+    console.log(e.latlng);
+}
+
+map.on('click', onMapClick);
+//Funcion para preguntar el nombre del marcador
+
+
+map.on('draw:created', function (e) {
+    var type = e.layerType,
+        layer = e.layer;
+        //Guardo las coordenadas en variables lat y lng para luego crear el objeto con las variables
+        var lat = layer.getLatLng().lat;
+        var lng = layer.getLatLng().lng;
+
+    if (type === 'marker') {
+        // Do marker specific actions
+    }
+
+    // Do whatever else you need to. (save to db, add to map etc)
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/";
+
+    MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mapa");
+    var myobj = { coordinates: [lat, lng], desc: "Punto x" };
+    dbo.collection("mapa").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("Coordenadas insertadas");
+        db.close();
+    });
+    });
+    map.addLayer(layer);
+});
+
