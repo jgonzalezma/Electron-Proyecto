2acda4b1f8b00a54dc4396629ef6f8131db6e9b5
jgonzalezma <josu0805@gmail.com>
2020-01-15 12:17:04 +0100

    markers, circles and circlemarkers

--- js/db.js
+++ js/db.js
@@ -1,13 +1,43 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
+//Recorrer markers
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mapa");
-  dbo.collection("mapa").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    var marker2 = L.marker(result[0].coordinates).addTo(map);
-    marker2.bindPopup("<b>"+result[0].desc+"</b>").openPopup();
-    db.close();
+    dbo.collection("marcadores").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      for(i = 0; i < result.length; i++){
+        var m = L.marker(result[i].coordinates).addTo(map);
+        m.bindPopup("<b>"+result[i].desc+"</b>").openPopup();
+      }
+      db.close();
+  });
+});
+
+//Recorrer CircleMarker
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mapa");
+    dbo.collection("circulos").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      for(i = 0; i < result.length; i++){
+        var m = L.circleMarker(result[i].coordinates).addTo(map);
+        m.bindPopup("<b>"+result[i].desc+"</b>").openPopup();
+      }
+      db.close();
+  });
+});
+//Recorrer Circle
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mapa");
+    dbo.collection("circulos").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      for(i = 0; i < result.length; i++){
+        var m = L.circle(result[i].coordinates).addTo(map);
+        m.bindPopup("<b>"+result[i].desc+"</b>").openPopup();
+      }
+      db.close();
   });
 });
--- js/map.js
+++ js/map.js
@@ -1,12 +1,9 @@
-var map = L.map('map', {drawControl:true}).setView([43.29, -1.98], 12);
+var map = L.map('map', {drawControl:true}).setView([43.29, -1.98], 11);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamdvbnphbGV6bWEiLCJhIjoiY2s1YzhrNmxuMDVjODNucWYyemxzaGZzOSJ9.8D3kII0_gLRI2XS3jKCDAA', {
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoiamdvbnphbGV6bWEiLCJhIjoiY2s1YzhrNmxuMDVjODNucWYyemxzaGZzOSJ9.8D3kII0_gLRI2XS3jKCDAA'
 }).addTo(map);
-
-//Marcadores
-var marker = L.marker([43.29, -1.98]).addTo(map);
 
 //Circulos
 var circle = L.circle([51.508, -0.11], {
@@ -23,9 +20,6 @@ var polygon = L.polygon([
 [51.51, -0.047]
 ]).addTo(map);
 
-//Popups
-marker.bindPopup("<b>Punto 1</b><br>I am a popup.").openPopup();
-
 //Ver escala
 L.control.scale().addTo(map);
 
@@ -36,32 +30,44 @@ function onMapClick(e) {
 map.on('click', onMapClick);
 //Funcion para preguntar el nombre del marcador
 
-
 map.on('draw:created', function (e) {
     var type = e.layerType,
         layer = e.layer;
         //Guardo las coordenadas en variables lat y lng para luego crear el objeto con las variables
         var lat = layer.getLatLng().lat;
         var lng = layer.getLatLng().lng;
+        var desc = "Punto y";
 
     if (type === 'marker') {
-        // Do marker specific actions
+        // Do whatever else you need to. (save to db, add to map etc)
+        var MongoClient = require('mongodb').MongoClient;
+        var url = "mongodb://localhost:27017/";
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mapa");
+            var myobj = { coordinates: [lat, lng], desc: desc };
+            dbo.collection("marcadores").insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                console.log(e);
+                db.close();
+            });
+            });
+        map.addLayer(layer);
     }
-
-    // Do whatever else you need to. (save to db, add to map etc)
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://localhost:27017/";
-
-    MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mapa");
-    var myobj = { coordinates: [lat, lng], desc: "Punto x" };
-    dbo.collection("mapa").insertOne(myobj, function(err, res) {
-        if (err) throw err;
-        console.log("Coordenadas insertadas");
-        db.close();
-    });
-    });
-    map.addLayer(layer);
+    else if (type === 'circle' || 'circlemarker'){
+        var MongoClient = require('mongodb').MongoClient;
+        var url = "mongodb://localhost:27017/";
+        var radius = layer.getRadius();
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mapa");
+            var myobj = { coordinates: [lat, lng], radius: radius, desc: desc };
+            dbo.collection("circulos").insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                console.log(e);
+                db.close();
+            });
+            });
+        map.addLayer(layer);
+    }
 });
-
