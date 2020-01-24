12ab702d757c4239544114dd1512f619c78a3b33
jgonzalezma <josu0805@gmail.com>
2020-01-24 12:17:34 +0100

    login validado falta cambiar estilo

--- js/validate.js
+++ js/validate.js
@@ -1,41 +1,30 @@
 //Validacion LOGIN de formulario con Jquery
-$(document).ready(function() {
-  console.log("hola");
-    $("#formulario").validate({
-      rules : {
-        username : {
-          maxlength : 3,
-          required : true
-        },
-  
-        password : {
-          maxlength : 20,
-          minlength : 8,
-          required : true
-        }
-  
-      }, //fin de las reglas, rules
-  
-      messages : {
-        username : {
-          maxlength : "Máximo 3 caracteres",
-          required : "Campo obligatorio"
-        },
-  
-        password : {
-          maxlength : "Máximo 20 caracteres",
-          minlength : "Mínimo de 8 carácteres",
-          required : "Campo obligatorio"
-        }
-      }//, //fin de los mensajes
-  
-      /*errorPlacement : function(error, element) {
-        if (element.is(":radio") || element.is(":checkbox")) {
-          error.appendTo(element.parent());
-        } else {
-          error.insertAfter(input);
-        }
-      }*/
-    }); //fin validate
-  }); //fin ready
+$("#formulario").validate({
+  rules : {
+    username : {
+      maxlength : 3,
+      required : true
+    },
+
+    password : {
+      maxlength : 20,
+      minlength : 8,
+      required : true
+    }
+
+  }, //fin de las reglas, rules
+
+  messages : {
+    username : {
+      maxlength : "Máximo 3 caracteres",
+  required : "Campo obligatorio"
+    },
+
+    password : {
+      maxlength : "Máximo 20 caracteres",
+      minlength : "Mínimo de 8 carácteres",
+      required : "Campo obligatorio"
+    }
+}, //fin de los mensajes
+}); //fin validate
   
