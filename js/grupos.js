57e5e35fa6878756e031727aa1cfd2ec038b9a64
jgonzalezma <josu0805@gmail.com>
2020-01-30 17:26:59 +0100

    muestra lista de grupos correctamente

--- js/grupos.js
+++ js/grupos.js
@@ -1,5 +1,16 @@
 //TODO Recorrer los grupos que ya están y ponerlos en la lista
-
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mapa");
+dbo.collection("grupos").find({}).toArray(function(err, result) {
+  if (err) throw err;
+  for(i = 0; i < result.length; i++){
+    grupos.push(result[i]);
+    $( "#contLista" ).append( "<li>"+result[i].nombre+"</li>" );
+  }
+  db.close();
+});
+});
 //Crear grupos a través del botón
 $("#crearBtn").click(function(){
     Swal.fire({
@@ -9,7 +20,6 @@ $("#crearBtn").click(function(){
     showCancelButton: true        
   }).then((result) => {
     if (result.value) {
-        //TODO insertar nombre de grupo en bd
         var MongoClient = require('mongodb').MongoClient;
         var url = "mongodb://localhost:27017/";
         var nombre = result.value;
